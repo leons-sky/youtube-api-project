37c7f781ead653aa1801c89c1353c7b16421d838
Ben Stirling <benjjstirling@gmail.com>
2022-10-28 09:58:39 +0100

    Hello

--- js/script.js
+++ js/script.js
@@ -9,35 +9,37 @@ function displayVideos(videos) {
         const element = document.createElement("article");
         element.classList.add("vidCard")
 
+
+        //adding the img to card. img's from snippet array 
         const img = document.createElement("img")
         img.src = video.snippet.thumbnails.high.url
         const text = document.createElement("h3")
+
         //text.classList.add("videoText")
         text.append(video.snippet.title)
         text.classList.add("videoText")
 
-
         // creating the like button, establishing an event listener
-        //when the button is triggered it turns green and back to white (toggle function)
         const like = document.createElement('button')
-        //like.setAttribute("onclick",)
         like.append('Like')
         element.append(img, text, like)
-        videosContainer.append(element)
+        div.append(element)
 
-        // when clicked (event listener) function, add the button to a class list and toggle the color
+        //when the button is triggered it turns green and back to white (toggle function)
         like.addEventListener("click", () => {
             like.classList.toggle("greenButton")
         })
-
+    
+        //when img of video is clicked, it will create a link via const url, and add the video.id number
         img.addEventListener("click", () => {
             const url = "https://www.youtube.com/watch?v=" + video.id
             window.location.href = url;
         })
-    }
-}
+    }   //NEED TO FIND WAY TO ADD NEW WINDOW 
+    // Styling search results (when searched)
 
-// Cycles through array of top 50 trending videos.
+} 
+// Search function 
 function search(input) {
     getTrendingVideos().then(videos => {
         let result = []
@@ -52,14 +54,14 @@ function search(input) {
                 flag = true
             }
             //search function cycles through array of tags per video.
-            //array can be 
+            //
             let tags = video.snippet.tags ?? []
             for (let tag of tags) {
                 if (tag.toLowerCase().includes(input.toLowerCase())) {
                     flag = true
                 }
             }
-
+          //search function will function against the channel title array against all character cases.
             let channelTitle = video.snippet.channelTitle
             if (channelTitle.toLowerCase().includes(input.toLowerCase())) {
                 flag = true
@@ -68,6 +70,7 @@ function search(input) {
             if (flag) result.push(video)
         } console.log(result)
 
+        // if search is no true - result err 
         displayVideos(result)
     }).catch(err => {
         console.log(err)
@@ -83,4 +86,8 @@ searchBar.addEventListener("input", () => {
 })
 
 
-// do suggested videos
+// POTENTIAL FUNCTIONALITY IDEAS:
+// Show total live likes (online + one local) on click 
+// Show video in ordered flex grid
+// display suggested videos when user logs in 
+// show list of liked videos, drop down menu 
